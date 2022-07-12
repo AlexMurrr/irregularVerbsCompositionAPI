b37c0d68220deb5122df0201bf4eed263eaf6207
Alex Mur <lehamuraschov2014@yandex.ru>
2022-07-13 00:29:13 +0200

    feat: add h2 and into test prettyDOM

--- src/components/h1V.test.js
+++ src/components/h1V.test.js
@@ -1,7 +1,9 @@
 import h1V from './h1V.vue';
-import { render } from '@testing-library/vue';
+import { render, prettyDOM } from '@testing-library/vue';
 
-test ('render', ()=> {
+test ('render h1V', ()=> {
    const {debag} = render(h1V)
    debag
+   console.log(prettyDOM());
 })
+
