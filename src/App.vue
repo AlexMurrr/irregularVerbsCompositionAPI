5f98b8fddcf025fc6f6d6f219f45994901c815d1
Alex Mur <lehamuraschov2014@yandex.ru>
2022-07-13 01:27:27 +0200

    feat: add vue component translateV.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
    import h1V from './components/h1V.vue'
+   import trancelate from "./components/trancelateV.vue"
 </script>
 
 <template>
   <main>
     <h1V/>
+    <trancelate/>
   </main>
 </template>
 
---
+++ src/components/trancelateV.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  Translate: &nbsp;&nbsp;
+  <input type="text" id="translate" placeholder="перевод" autofocus />
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  div{
+  font-weight: 400;
+  line-height: 1.45;
+  font-size: 1.6rem;
+  font-weight: 700;
+  }
+</style>
