4bffd86dcdc97670d7736a8cea6625910265d26f
Alex Mur <lehamuraschov2014@yandex.ru>
2022-09-15 23:23:03 +0200

    feat: add style for button

--- src/components/formVerb.vue
+++ src/components/formVerb.vue
@@ -80,13 +80,13 @@ function checkMethods(){
     v-model="partiziple">
     <p class="p" v-html="resultSimpleParticiple"></p>
       <br/>
-    <button type="submit" v-if="isTrue" @click="isTrue=!isTrue,
+    <button class="btn" type="submit" v-if="isTrue" @click="isTrue=!isTrue,
                                                 rendVerb(),
                                                 resetVaueRes(),
                                                 resetValueInput()"
                                                 >Start</button>
                                                 
-    <button type="submit" v-else @click=" isTrue=!isTrue;             
+    <button class="btn" type="submit" v-else @click=" isTrue=!isTrue;             
           check(removeEmptySpace(tranclate),removeEmptySpace(translateTrue));
           past(removeEmptySpace(pastSimple), removeEmptySpace(pastSimpleTrue));
           participle(removeEmptySpace(partiziple), removeEmptySpace(partizipleTrue));
@@ -105,5 +105,35 @@ function checkMethods(){
 .label{
   color: #17173f;
   
+}
+.btn{
+  font-size: 18px;
+    text-decoration: none;
+    cursor: pointer;
+    display:block;
+        width:130px;
+    font-weight: bold;
+    margin: auto;
+        text-align:center;
+    padding: 7px 5px;
+    color: #717171;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+    text-shadow: 0 1px 0 rgba(255, 255, 255, 0.3);
+    -webkit-box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.2), inset 0 0 6px 0 rgba(255, 255, 255, 0.3), 0 1px 2px rgba(0, 0, 0, 0.4);
+    -moz-box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.2), inset 0 0 6px 0 rgba(255, 255, 255, 0.3), 0 1px 2px rgba(0, 0, 0, 0.4);
+    box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.2), inset 0 0 6px 0 rgba(255, 255, 255, 0.3), 0 1px 2px rgba(0, 0, 0, 0.4);
+    background: #f6f6f6;    
+    background: -webkit-linear-gradient(#f6f6f6, #d4d4d4);
+    background-image: -moz-linear-gradient(top, #f6f6f6, #d4d4d4);
+    background-image: -moz-gradient(top, #f6f6f6, #d4d4d4);
+    border: 1px solid #a1a1a1;
+}
+.btn:hover{
+   background: #f6f6f6;    
+    background: -webkit-linear-gradient(#f6f6f6, #ececec);
+    background-image: -moz-linear-gradient(top, #f6f6f6, #ececec);
+    background-image: -moz-gradient(top, #f6f6f6, #ececec);
 }
 </style>
