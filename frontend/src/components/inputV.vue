e77174590ef33144d265b06a15b913d4998ab324
Alex Mur <lehamuraschov2014@yandex.ru>
2023-06-27 15:32:45 +0200

    feat: create component 'inputV'

---
+++ frontend/src/components/inputV.vue
@@ -0,0 +1,37 @@
+<script setup>
+    const props = defineProps({
+        value: {
+            type: String,
+            default: '',
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            default: 'text'
+        },
+        placeholder: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+    });
+</script>
+
+<template>
+    <div>
+      <input 
+      class="input-text"
+      :value=value
+      :name=name
+      :type=type
+      :placeholder=placeholder
+      >
+      <label :for='name' class="input-label" >{{ label }}</label>     
+    </div>
+  </template>
