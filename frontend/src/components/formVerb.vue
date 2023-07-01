b8d8fdbb0dfdb73de644c784fc772eea25762656
Alex Mur <lehamuraschov2014@yandex.ru>
2023-07-01 15:20:20 +0200

    feat: create validators to translate, pastSimple, partiziple

--- frontend/src/components/formVerb.vue
+++ frontend/src/components/formVerb.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { ref } from "vue";
+import { ref,computed } from "vue";
 import { useStoreVerbs } from "../stores/verbs";
 import { storeCheckVerbs } from "../stores/checkVerbs";
 import { storeToRefs } from "pinia";
 import  buttonV from "./buttonV.vue";
-import inputV from "./inputV.vue"
+import inputV from "./inputV.vue";
+import useVuelidate from "@vuelidate/core";
+import {minLength} from "@vuelidate/validators";
 
 const storeVerbs = useStoreVerbs();
 
@@ -60,6 +62,20 @@ async function callAsyncFunToStart() {
   await getValueFormVerbs();
 }
 
+const rules = computed(()=>({
+  tranclate: {
+    minLength: minLength(3)
+  },
+  pastSimple: {
+    minLength: minLength(3)
+  },
+  partiziple: {
+    minLength: minLength(3)
+  },
+}))
+
+const validate = useVuelidate(rules, {tranclate, pastSimple, partiziple});
+
 </script>
 
 <template>
@@ -69,7 +85,8 @@ async function callAsyncFunToStart() {
       <label class="label"><strong>Translate:</strong></label> <br />
 
       <inputV
-      v-model:value="tranclate"
+      v-model:value="validate.tranclate.$model"
+      :errorTranslate="validate.tranclate.$errors"
       placeholder="Введите перевод"
       name='tranclate'
       />
@@ -79,9 +96,10 @@ async function callAsyncFunToStart() {
       <!-- eslint-enable -->
 
       <label class="label"><strong>Simple Past:</strong></label> <br />
-
-      <inputV
-      v-model:value="pastSimple"
+     
+      <inputV     
+      v-model:value="validate.pastSimple.$model"  
+      :errorPastSimple="validate.pastSimple.$errors"    
       placeholder="Введите простое прошедшее"
       name='pastSimple'
       />
@@ -93,11 +111,12 @@ async function callAsyncFunToStart() {
       <label class="label"><strong> Simple Participle:</strong></label> <br/>
 
       <inputV
-      v-model:value="partiziple"
+      v-model:value="validate.partiziple.$model"
+      :errorPastSimple = "validate.partiziple.$errors"
       placeholder="Введите простое причастие"
       name='partiziple'
       /> 
-            
+
       <!-- eslint-disable -->
       <p v-html="resultSimpleParticiple"></p>
       <!-- eslint-enable -->
--- frontend/src/components/inputV.vue
+++ frontend/src/components/inputV.vue
@@ -1,9 +1,24 @@
 <script setup>
     const emit = defineEmits(['update:value']);
     const props = defineProps({
+        errorTranslate: {
+            type: Array,
+            required: false
+        },
+
+        errorPastSimple: {
+            type: Array,
+            required: false
+        },
+
+        errorPartiziple: {
+            type: Array,
+            required: false
+        },
+
         value: {
             type: String,
-            default: '',
+            default: '',            
         },
         name: {
             type: String,
@@ -35,13 +50,34 @@ const updateValue = (e) => {
       :type=type
       :placeholder=placeholder
       @input="updateValue"
-      >           
-    </div>
+      >
+    <div class="form-error"
+    v-for="element of errorTranslate"
+    :key="element.$uid"
+    >
+    <div class="errMessage">{{ element.$message }}</div>        
+    </div>          
+    <div class="form-error"
+    v-for="element of errorPastSimple"
+    :key="element.$uid"
+    >
+    <div class="errMessage">{{ element.$message }}</div>        
+    </div>  
+    <div class="form-error"
+    v-for="element of errorPartiziple"
+    :key="element.$uid"
+    >
+    <div class="errMessage">{{ element.$message }}</div>        
+    </div>  
+    </div>        
   </template>
 
 <style>
-    .input {
+  .input {
   width: 450px;
   margin: 20px auto;
+}
+.errMessage{
+    color: red;
 }
 </style>
